5bd6997ce028951aee1709094c2f39ad6804a48d
John King <John.A.King-1@ou.edu>
2020-09-08 20:31:32 -0500

    commit 1

---
+++ Homework1/Directories_Files/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Directories_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
